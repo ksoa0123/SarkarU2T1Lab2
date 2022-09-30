51d32c8b7a4f2097f36982cc4bd0464f2a8f9b82
Koushik Sarkar <koushiks@nycstudents.net>
2022-09-30 12:32:26 -0400

    Fixed File

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,9 +1,8 @@
 public class RectangleRunner {
-    Rectangle rect1 = new Rectangle(10, 20);
+    public static void main(String[] args){
+        Rectangle rect1 = new Rectangle(10, 20);
 
-    rect1.pritnArea();
-    rect1.printBoxVolume(30);
-
-
-
+        rect1.printArea();
+        rect1.printBoxVolume(30);
+    }
 }
